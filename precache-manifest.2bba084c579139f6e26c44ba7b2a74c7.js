eb1b7cbf8e64e21d4afe6272f21758a1ff2ab955
Bachar <Bachar@mbp-de-michel.home>
2019-04-07 18:00:49 +0200

    Updates

---
+++ precache-manifest.2bba084c579139f6e26c44ba7b2a74c7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1d5f0205ef0b554129ef",
+    "url": "/mdm/static/js/runtime~main.07f3abb0.js"
+  },
+  {
+    "revision": "911b1b409c06f84e8155",
+    "url": "/mdm/static/js/main.df59685e.chunk.js"
+  },
+  {
+    "revision": "f029c3f640e94c1a7049",
+    "url": "/mdm/static/js/2.460d3ee7.chunk.js"
+  },
+  {
+    "revision": "911b1b409c06f84e8155",
+    "url": "/mdm/static/css/main.93cb9d9e.chunk.css"
+  },
+  {
+    "revision": "6ba7348fbe9208e6ef5fac1217924c6e",
+    "url": "/mdm/index.html"
+  }
+];
